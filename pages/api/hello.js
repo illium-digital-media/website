fbfd2a0d0a89f2d7cadabb0d04f1c11d43a22852
clayharrison8 <clayharrison8@gmail.com>
2023-07-20 00:04:34 +0100

    fixing package.json issues

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,15 @@
-export {};
+// Replace "export {};" with CommonJS equivalent:
+module.exports = {};
 
 // Your existing code here...
-import express from 'express';
-import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
-import cors from 'cors';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
-
-app.post('/send-email', (req, res) => {
-  // Your route handling code here
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
