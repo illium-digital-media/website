007840f7d27afd02a53b77b976899346fbea918e
clayharrison8 <clayharrison8@gmail.com>
2023-07-20 00:09:10 +0100

    removing server side code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,17 @@
-// Replace "export {};" with CommonJS equivalent:
-module.exports = {};
+// // Replace "export {};" with CommonJS equivalent:
+// module.exports = {};
 
-// Your existing code here...
-const express = require('express');
-const bodyParser = require('body-parser');
+// // Your existing code here...
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const app = express();
-const port = 3001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = express();
+// const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
