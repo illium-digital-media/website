4e649f82bdaa5815aa1011516f7e258de39246dd
clayharrison8 <clayharrison8@gmail.com>
2023-08-01 02:45:26 +0100

    fixing photogallery build bug

--- pages/PhotoGallery.tsx
+++ pages/PhotoGallery.tsx
@@ -1,28 +1,32 @@
 import Image, { StaticImageData } from "next/image";
 import React, { useState } from "react";
 
-const PhotoGallery: React.FC<{images: string[] | StaticImageData[]}> = (props) => {
+const PhotoGallery: React.FC<{ images: string[] | StaticImageData[] }> = (
+  props
+) => {
   const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
 
   const handleNextImage = () => {
-    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % props.images.length);
+    setSelectedImageIndex((prevIndex) =>
+      (prevIndex + 1) % (props.images?.length || 1)
+    );
   };
 
   const handlePrevImage = () => {
     setSelectedImageIndex((prevIndex) =>
-      prevIndex === 0 ? props.images.length - 1 : prevIndex - 1
+      prevIndex === 0 ? (props.images?.length || 1) - 1 : prevIndex - 1
     );
   };
 
   return (
     <div className="relative">
       <Image
-        src={props.images[selectedImageIndex]}
+        src={props.images?.[selectedImageIndex]}
         alt={`Image ${selectedImageIndex + 1}`}
         className="cursor-pointer rounded-xl"
         onClick={handleNextImage}
       />
-      {props.images.length > 1 && (
+      {props.images?.length && props.images.length > 1 && (
         <>
           <span
             className="absolute top-1/2 left-4 text-white text-4xl cursor-pointer"
