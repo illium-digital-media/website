edd8820c174d4f903e1a0ace4a19d967d4f6dc86
clay <clayborne.harrison@sykescottages.co.uk>
2023-08-28 04:29:09 +0100

    added progress bar

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const ProgressBar = () => {
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPosition = window.pageYOffset;
+
+      if (totalHeight !== 0) {
+        setScrollPercentage((scrollPosition / totalHeight) * 100);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 h-1.5 w-full z-50">
+      <div
+        className="h-full bg-cyan-500 transition-all duration-200"
+        style={{ width: `${scrollPercentage}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
 import MainNavigation from "@/components/Navigation/MainNavigation";
+import ProgressBar from "@/components/ProgressBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
         <MainNavigation />
+        <ProgressBar/>
+
         <Component {...pageProps} />
         <Footer />
     </>
