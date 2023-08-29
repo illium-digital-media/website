1815003f4684fd1c4c0f8e7b14ee85523b2132ad
clay <clayborne.harrison@sykescottages.co.uk>
2023-08-29 09:58:56 +0100

    updated faq content

--- data/faqs.tsx
+++ data/faqs.tsx
@@ -3,64 +3,60 @@ export const faqs = [
     category: "Services",
     questions: [
       {
-        question: "What services does Illium Digital Designs offer?",
+        question: "What services does Illium Digital Media offer?",
         answer:
-          "We offer a wide range of services, including web design, web development, digital marketing, branding, and more.",
+          "llium Digital Media offers a range of services including web development, UX design, copywriting, SEO, lifetime updates, and digital marketing. Our team is committed to creating captivating digital experiences that drive growth and engagement. With expertise in crafting websites, enhancing user experiences, creating compelling content, improving online visibility, and navigating the dynamic digital landscape, we ensure your brand remains at the forefront of innovation and success.",
       },
       {
-        question: "Can you design and develop e-commerce websites?",
+        question: "What software do you use to make websites?",
         answer:
-          "Yes, we specialize in creating e-commerce websites with user-friendly interfaces and secure payment gateways.",
+          "We specialize in website development using a modern tech stack that includes React, TypeScript, Next.js, and Tailwind CSS. These technologies allow us to create dynamic, fast, and responsive websites that provide exceptional user experiences. Our team harnesses the power of these tools to craft custom solutions that align with your unique business goals and requirements.",
       },
       {
-        question: "Do you provide custom solutions for businesses?",
+        question: "Which software tools do you utilize for your design work?",
         answer:
-          "Absolutely! We tailor our services to meet the unique needs and goals of each business.",
+          "We transform clients' visions into captivating digital experiences using a combination of creative tools. Our design process begins with harnessing the capabilities of Figma, where we collaboratively craft concepts and turn ideas into tangible designs. This thoughtful approach enables us to bring your dreams to life by ensuring that every detail is meticulously captured and refined throughout the design journey.",
       },
-      // ... (other questions for the Services category)
+      {
+        question: "I currently have a website and I'm looking to upgrade it?",
+        answer:
+          "If you're looking to upgrade your existing website, Illium Digital Media is your solution. We specialize in enhancing websites with modern design, improved functionality, and updated features. With our expertise in web development, user experience, and digital marketing, we'll transform your online presence to make a lasting impact.",
+      },
     ],
   },
   {
     category: "Pricing and Deadlines",
     questions: [
       {
-        question: "How do you determine the cost of a project?",
+        question: "How does your pricing structure work for projects?",
         answer:
-          "Our pricing varies based on the scope of the project, features required, and estimated development time.",
-      },
-      {
-        question: "Do you offer different pricing packages?",
-        answer:
-          "Yes, we provide different pricing packages to accommodate businesses of all sizes.",
+          "We offer a unique subscription model designed to provide flexibility and affordability for our clients. For a standard 5-page website, we offer a subscription of £50 per month. This includes the core features and design elements to get your online presence up and running. If your project requires additional pages or features beyond the initial scope, we provide tailored pricing based on the specific scope and complexity. Our goal is to ensure you get the most value for your investment while accommodating your project's individual needs.",
       },
       {
         question: "What are the typical turnaround times for projects?",
         answer:
-          "The turnaround time depends on the complexity of the project. We provide estimated timelines during the consultation.",
+          "At Illium Digital Media, our project turnaround times are designed to be efficient and tailored to the specific scope and complexity of each project. For our subscription-based 5-page websites, we aim to deliver within a few weeks. However, turnaround times can vary based on factors such as additional features, customisation, and the overall project size. Rest assured, we prioritise delivering high-quality results promptly without compromising on excellence.",
       },
-      // ... (other questions for the Pricing and Deadlines category)
     ],
   },
   {
     category: "About Us",
     questions: [
       {
-        question: "Who is behind Illium Digital Designs?",
+        question: "Who is behind Illium Digital Media?",
         answer:
-          "Illium Digital Designs was founded by passionate professionals with years of experience in web design and development.",
+          "Illium Digital Media is powered by a team of experienced professionals well-versed in the art of web development. With a rich background in crafting digital experiences, we are committed to transforming ideas into stunning online realities. Our focus is on delivering accessible and high-quality web presence for businesses of all sizes.",
       },
       {
-        question: "What is your approach to client collaboration?",
+        question: "How do you engage with clients during the creative process?",
         answer:
-          "We believe in open communication and collaboration. We work closely with clients to ensure their vision is realized.",
+          "Our approach to client collaboration is rooted in open communication and a deep understanding of your vision. We believe in active partnership throughout the entire process, from concept to launch. We listen to your ideas, gather insights, and provide expert guidance to shape your digital presence. Our collaborative process ensures that your unique needs and preferences are at the forefront, resulting in a website that truly reflects your brand identity and goals.",
       },
       {
         question: "How do you stay updated with industry trends?",
         answer:
-          "Our team stays up-to-date with the latest industry trends and technologies to provide innovative solutions.",
+          "We stay updated with industry trends through comprehensive research, leveraging online resources, and maintaining connections with industry experts.",
       },
-      // ... (other questions for the About Us category)
     ],
   },
-  // ... (other categories)
 ];
