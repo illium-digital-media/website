c2dc76ae9d154eaab10c6d68c241cb2bf911ff29
clay <clayborne.harrison@sykescottages.co.uk>
2023-08-30 20:08:20 +0100

    seperating contact us slide in refs

--- components/sections/ContactUsSection.tsx
+++ components/sections/ContactUsSection.tsx
@@ -3,36 +3,36 @@ import FadeInText from "../FadeInTypingText";
 import useVisibilityOnScroll from "@/hooks/useVisibilityonScroll";
 
 const ContactUsSection = () => {
-  const { isVisible, sectionRef } = useVisibilityOnScroll();
+  const { isVisible: isFormVisible, sectionRef: formRef } = useVisibilityOnScroll();
+  const { isVisible: isContentVisible, sectionRef: contentRef } = useVisibilityOnScroll();
+  const { isVisible: isTitleVisible, sectionRef: titleContainerRef } = useVisibilityOnScroll();
 
   return (
     <div className="overflow-x-hidden">
-      <div ref={sectionRef}>
-        <div className="flex max-sm:block max-w-screen-lg m-auto">
-          <div className={`sm:hidden pb-10 transform ${isVisible ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500`}>
-            <h3 className={`font-bold text-white text-center pb-8 text-4xl`}>
-              <FadeInText text="Connect with Us" />
-            </h3>
-            <p className="text-gray-300">
-              Whether you&apos;re on the cusp of launching a groundbreaking digital project or just pondering the potential of an idea, we&apos;re here to guide and assist. From detailed inquiries to simple questions, every message is important to us. Reach out, and let&apos;s start a conversation that could shape the future of your digital presence. We promise a prompt and thoughtful response.
-            </p>
-          </div>
-          <div className={`sm:pr-20 sm:w-1/2 transform ${isVisible ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-500`}>
+      <div>
+        <div className="flex flex-col-reverse sm:flex-row max-w-screen-lg m-auto">
+          <div 
+            ref={formRef} 
+            className={`w-full sm:w-1/2 sm:pr-20 transform ${isFormVisible ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-500`}
+          >
             <ContactForm />
           </div>
-          <div className={`max-sm:hidden sm:w-1/2 transform ${isVisible ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500`}>
-            <h3 className={`font-bold text-white pb-8 text-4xl`}>
-              <FadeInText text="Connect with Us" />
-            </h3>
+          <div 
+            ref={contentRef} 
+            className={`w-full sm:w-1/2 pb-10 transform ${isContentVisible ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500`}
+          >
+            <div ref={titleContainerRef} className={`${isTitleVisible ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-500`}>
+              <h3 className={`font-bold text-white max-sm:text-center pb-8 text-4xl`}>
+                <FadeInText text="Connect with Us" />
+              </h3>
+            </div>
             <p className="text-gray-300">
               Whether you&apos;re on the cusp of launching a groundbreaking digital project or just pondering the potential of an idea, we&apos;re here to guide and assist. From detailed inquiries to simple questions, every message is important to us. Reach out, and let&apos;s start a conversation that could shape the future of your digital presence. We promise a prompt and thoughtful response.
             </p>
           </div>
         </div>
-      </div>Ï
+      </div>
     </div>
-
-
   );
 }
 
