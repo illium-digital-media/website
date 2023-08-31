a91aa419a6b5c66d5a6c5be76feb40a395e8b7d4
clay <clayborne.harrison@sykescottages.co.uk>
2023-08-31 10:43:01 +0100

    optimising portfolio images

---
+++ components/ProjectDisplay.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import RightUpArrowIcon from './Icons/RightUpArrow';
+import { motion } from 'framer-motion';
+import { projects } from '@/data/projects';
+
+interface ProjectDisplayProps {
+    activeSite: string;
+}
+
+const ProjectDisplay: React.FC<ProjectDisplayProps> = ({ activeSite }) => {
+    const imageVariants = {
+        hidden: { opacity: 0, scale: 0.9 },
+        visible: { opacity: 1, scale: 1 }
+    };
+
+    return (
+        <div className={`sm:w-1/2 bg-secondary rounded-lg flex items-center justify-center`}>
+            <div className="w-full h-full rounded-md bg-gradient-to-r from-secondary to-cyan-400">
+                {projects.map((project, index) => (
+                    activeSite === project.id &&
+                    <motion.div 
+                        key={index}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                        variants={imageVariants}
+                        transition={{ duration: 0.9 }}
+                        className="w-full h-full rounded-md bg-gradient-to-r from-secondary to-cyan-400 p-0.5"
+                    >
+                        <div className="h-full w-full bg-gradient-to-r from-secondary via-tertiary to-secondary p-5 max-sm:p-3">
+                            <div className="relative">
+                                <a href={project.url} target="_blank" className="absolute top-0 right-0  bg-transparent">
+                                    <div className="text-orange-600 cursor-pointer hover:scale-110">
+                                        <RightUpArrowIcon />
+                                    </div>
+                                </a>
+                                <link rel="prefetch" href={project.imagePath.src} as="image" />
+                                                                <Image
+                                    key={project.id}
+                                    alt={project.name}
+                                    src={project.imagePath}
+                                    className="transition-opacity duration-500 max-sm:p-10 sm:p-20"
+                                />
+                                <div className="text-white">{project.name}</div>
+                            </div>
+                        </div>
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ProjectDisplay;
--- components/sections/PortfolioSection.tsx
+++ components/sections/PortfolioSection.tsx
@@ -1,28 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import Image, { StaticImageData } from 'next/image';
+import React, { useState, useEffect, useMemo } from 'react';
+import { StaticImageData } from 'next/image';
 import FadeInText from '../FadeInTypingText';
-import RightUpArrowIcon from '../Icons/RightUpArrow';
 import ProjectSelector from '../ProjectSelector';
 import useVisibilityOnScroll from "@/hooks/useVisibilityonScroll";
-import { motion } from 'framer-motion';
 import { projects } from '@/data/projects';
+import ProjectDisplay from '../ProjectDisplay';
+
+const ProjectList: React.FC<{ activeSite: string; setActiveSite: (id: string) => void }> = ({ activeSite, setActiveSite }) => {
+    return (
+        <div className='w-3/4'>
+            {projects.map((project) => (
+                <div key={project.id}>
+                    <div onClick={() => setActiveSite(project.id)}
+                        className={`p-4 hover:text-cyan-400 cursor-pointer ${activeSite === project.id ? 'font-bold text-cyan-400' : 'text-white'}`}
+                    >
+                        {project.name}
+                    </div>
+                    <hr className="bg-gradient-to-l from-transparent via-cyan-400 to-transparent h-px border-none max-w-screen-lg m-auto" />
+                </div>
+            ))}
+        </div>
+    );
+};
 
 const PortfolioSection: React.FC = () => {
     const [activeSite, setActiveSite] = useState<string>(projects[0].id);
-    const [activeImage, setActiveImage] = useState<StaticImageData>(projects[0].imagePath);
     const { isVisible, sectionRef } = useVisibilityOnScroll();
-
-    useEffect(() => {
-        const selectedProject = projects.find((project) => project.id === activeSite);
-        if (selectedProject) {
-            setActiveImage(selectedProject.imagePath);
-        }
-    }, [activeSite]);
-
-    const imageVariants = {
-        hidden: { opacity: 0, scale: 0.9 },
-        visible: { opacity: 1, scale: 1 }
-    };
 
     return (
         <div className="overflow-x-hidden">
@@ -30,52 +33,9 @@ const PortfolioSection: React.FC = () => {
                 <h2 className="text-4xl font-bold text-center text-white pb-10"><FadeInText text='Our Latest Projects' /></h2>
                 <div className="sm:flex">
                     <div className={`max-sm:hidden w-1/2 flex items-center transition-transform duration-500 ${isVisible ? 'translate-x-0' : '-translate-x-full'}`}>
-                        <div className='w-3/4'>
-                            {projects.map((project) => (
-                                <div key={project.id}>
-                                    <div onClick={() => setActiveSite(project.id)}
-                                        className={`p-4 hover:text-cyan-400 cursor-pointer ${activeSite === project.id ? 'font-bold text-cyan-400' : 'text-white'}`}
-                                    >
-                                        {project.name}
-                                    </div>
-                                    <hr className="bg-gradient-to-l from-transparent via-cyan-400 to-transparent h-px border-none max-w-screen-lg m-auto" />
-                                </div>
-                            ))}
-                        </div>
+                        <ProjectList activeSite={activeSite} setActiveSite={setActiveSite} />
                     </div>
-                    <div className={`sm:w-1/2 bg-secondary rounded-lg flex items-center justify-center transition-transform duration-500 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
-                        <div className="w-full h-full rounded-md bg-gradient-to-r from-secondary to-cyan-400">
-                            {projects.map((project, index) => (
-                                activeSite === project.id &&
-                                <motion.div 
-                                    key={index}
-                                    initial="hidden"
-                                    animate="visible"
-                                    exit="hidden"
-                                    variants={imageVariants}
-                                    transition={{ duration: 0.9 }}
-                                    className="w-full h-full rounded-md bg-gradient-to-r from-secondary to-cyan-400 p-0.5"
-                                >
-                                    <div className="h-full w-full bg-gradient-to-r from-secondary via-tertiary to-secondary p-5 max-sm:p-3">
-                                        <div className="relative">
-                                            <a href={project.url} target="_blank" className="absolute top-0 right-0  bg-transparent">
-                                                <div className="text-orange-600 cursor-pointer hover:scale-110">
-                                                    <RightUpArrowIcon />
-                                                </div>
-                                            </a>
-                                            <Image
-                                                key={project.id}
-                                                alt={project.name}
-                                                src={activeImage}
-                                                className="transition-opacity duration-500 max-sm:p-10 sm:p-20"
-                                            />
-                                            <div className="text-white">{project.name}</div>
-                                        </div>
-                                    </div>
-                                </motion.div>
-                            ))}
-                        </div>
-                    </div>
+                    <ProjectDisplay activeSite={activeSite} />
                     <div className='sm:hidden'>
                         <ProjectSelector activeSite={activeSite} setActiveSite={setActiveSite} />
                     </div>
