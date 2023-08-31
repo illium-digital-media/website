b18e4af7c1a15ff193a949b97b5f7425feeac9fb
clay <clayborne.harrison@sykescottages.co.uk>
2023-08-31 08:08:06 +0100

    fixing z-index issue

--- components/sections/CommonLanding.tsx
+++ components/sections/CommonLanding.tsx
@@ -14,7 +14,7 @@ const CommonLanding: React.FC<{ heading: string }> = (props) => {
       </div>
 
       {/* Wrapper for content to maintain max-width */}
-      <div className="max-w-screen-xl m-auto w-full z-10 flex justify-center">
+      <div className="max-w-screen-xl m-auto w-full z-0 flex justify-center">
         <h3 className="text-5xl font-bold max-lg:text-4xl text-white"><FadeInText text={props.heading} /></h3>
       </div>
     </section>
