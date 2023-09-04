f6f9ab9aab095828e7d6bb1d8446e060e7f22d30
clay <clayborne.harrison@sykescottages.co.uk>
2023-09-04 22:20:32 +0100

    updating favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Exo 2:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/logo-short-dark.ico" media="(prefers-color-scheme: light)" />
-        <link rel="icon" href="/logo-short-light.ico" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" href="/short-logo.ico" media="(prefers-color-scheme: dark)" />
       </Head>
       <body className='bg-primary'>
         <Main />
