838e41d8b87f6d38d0e3f847ddefacf3cfaff8d4
clay <clayborne.harrison@sykescottages.co.uk>
2023-09-04 22:26:08 +0100

    updating mobile nav spacing

--- components/Navigation/NavItem.tsx
+++ components/Navigation/NavItem.tsx
@@ -12,7 +12,7 @@ const NavItem: React.FC<{
     <>
       <div className={`${isActive ? 'text-cyan-400 max-sm:font-bold sm:border-b-2 border-cyan-400' : 'text-white'} sm:h-full sm:flex items-center`}>
         <Link href={props.pageLink} onClick={props.handleClick}>
-          <span className={`px-5 max-sm:py-7 m-auto hover:text-cyan-400 duration-200 max-sm:flex sm:justify-center`}>
+          <span className={`px-5 max-sm:py-5 m-auto hover:text-cyan-400 duration-200 max-sm:flex sm:justify-center`}>
             {props.copy}
           </span>
         </Link>
